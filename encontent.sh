7c95cc55ff4c16e9e28b861a1a6592386bc8838c
Jeremy Schwartz <jeremy@worldpossible.org>
2015-02-13 10:10:33 -0800

    full file path for module download

--- encontent.sh
+++ encontent.sh
@@ -1,20 +1,20 @@
 sudo mkdir /var/www/modules
-cd /var/www/modules/
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/ebooks-en ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/hesperian_health ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/iicba ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/infonet ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/kaos-en ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/math_expression ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/medline_plus ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/medline_plus ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/musictheory ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/olpc ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/powertyping ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/practical_action ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/scratch ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/understanding_algebra ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/wikipedia_for_schools ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/rpi_guide ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/windows_apps ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/asst_medical ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/ebooks-en /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/hesperian_health /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/iicba /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/infonet /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/kaos-en /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/math_expression /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/medline_plus /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/medline_plus /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/musictheory /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/olpc /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/powertyping /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/practical_action /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/scratch /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/understanding_algebra /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/wikipedia_for_schools /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/rpi_guide /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/windows_apps /var/www/modules
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/asst_medical /var/www/modules
+rm -r /home/pi/rachelpiOS/www
