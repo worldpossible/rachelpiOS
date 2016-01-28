df4f32c0995ff596862e3c1be4ee3ca960270767
Richard Segal <rsegal@us.ibm.com>
2016-01-28 13:34:57 -0500

    Added option to encontent.sh to specify an alternative rsync source

--- files/encontent.sh
+++ files/encontent.sh
@@ -9,10 +9,46 @@ DEST_PATH=/var/www/modules
 RSYNC_SOURCE=rsync://dev.worldpossible.org/rachelmods/
 
 CMDNAME=$0
+Usage () {
+    echo "$CMDNAME [options]"
+    echo "  Options:"
+    echo "    --source       RACHEL content server. (default '$RSYNC_SOURCE')"
+    echo "    --help         Display help message"
+    echo ""
+}
+
+while [ true ]; do
+    case $1 in 
+        -h | --help | -\?)
+            Usage
+            exit 0
+            ;;
+        --source) 
+            RSYNC_SOURCE="$2"
+            shift 2
+            ;;
+	--*)
+	    echo "$CMDNAME: invalid option '$1'"
+	    exit 1
+	    ;;
+        *) 
+            break
+            ;;
+    esac
+done
+
+if [ $# -gt 0 ]; then
+    echo "$CMDNAME: too many arguments"
+    exit 1
+fi
 
 if [ `id -u` != "0" ]; then
     echo "$CMDNAME: This script must be run as root"
     exit
+fi
+
+if [ "${RSYNC_SOURCE: -1}" != ":" ] && [ "${RSYNC_SOURCE: -1}" != "/" ]; then
+    RSYNC_SOURCE="${RSYNC_SOURCE}/"
 fi
 
 for module in ${MODULES[@]}; do
