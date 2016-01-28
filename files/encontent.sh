167b85cd90917f2d87c8f37ae486104ecb76c7e2
Richard Segal <rsegal@us.ibm.com>
2016-01-28 15:27:20 -0500

    Addded several options to encontent.sh to support working with local content mirrors

--- files/encontent.sh
+++ files/encontent.sh
@@ -7,12 +7,18 @@ MODULES=( ebooks-en hesperian_health iicba infonet kaos-en math_expression \
 
 DEST_PATH=/var/www/modules
 RSYNC_SOURCE=rsync://dev.worldpossible.org/rachelmods/
+FILE_OWNER="www-data"
+RSYNC_OPT=()
 
 CMDNAME=$0
 Usage () {
     echo "$CMDNAME [options]"
     echo "  Options:"
+    echo "    --dest         Path to store downloaded RACHEL content (default '$DEST_PATH')"
+    echo "    --rsh          Set rsync shell command"
     echo "    --source       RACHEL content server. (default '$RSYNC_SOURCE')"
+    echo "    --own          Keep ownership of copied files"
+    echo "    --owner        Set owner of copied files"
     echo "    --help         Display help message"
     echo ""
 }
@@ -27,6 +33,22 @@ while [ true ]; do
             RSYNC_SOURCE="$2"
             shift 2
             ;;
+	--dest)
+	    DEST_PATH="$2"
+	    shift 2
+	    ;;
+        --own) 
+	    FILE_OWNER=""
+            shift 1
+            ;;
+        --owner) 
+	    FILE_OWNER="$2"
+            shift 2
+            ;;
+	--rsh)
+	    RSYNC_OPT=("-e" "$2")
+	    shift 2
+	    ;;
 	--*)
 	    echo "$CMDNAME: invalid option '$1'"
 	    exit 1
@@ -42,7 +64,7 @@ if [ $# -gt 0 ]; then
     exit 1
 fi
 
-if [ `id -u` != "0" ]; then
+if [ `id -u` != "0" ] && [ ! -z "$FILE_OWNER" ]; then
     echo "$CMDNAME: This script must be run as root"
     exit
 fi
@@ -51,10 +73,16 @@ if [ "${RSYNC_SOURCE: -1}" != ":" ] && [ "${RSYNC_SOURCE: -1}" != "/" ]; then
     RSYNC_SOURCE="${RSYNC_SOURCE}/"
 fi
 
+if [ -z "$FILE_OWNER" ]; then
+    CHOWN_CMD=""
+else
+    CHOWN_CMD="sudo -u $FILE_OWNER SSHPASS=$SSHPASS "
+fi
+
 for module in ${MODULES[@]}; do
     echo
     echo "Syncing $module ..."
-    sudo -u www-data rsync -rlptvz "${RSYNC_SOURCE}${module}" "$DEST_PATH"
+    ${CHOWN_CMD}rsync -rlptvz "${RSYNC_OPT[@]}" "${RSYNC_SOURCE}${module}" "$DEST_PATH"
     if [ $? -ne 0 ]; then
 	sleep 1
 	echo "Copying of $module failed or was aborted.  Exiting."
