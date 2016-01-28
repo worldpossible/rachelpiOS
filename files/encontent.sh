eabca11a5523fe46b7e5487a5cf658643d967066
Richard Segal <rsegal@us.ibm.com>
2016-01-28 11:57:21 -0500

    Reorganized content loading code into a loop

--- files/encontent.sh
+++ files/encontent.sh
@@ -1,20 +1,27 @@
-sudo mkdir /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/ebooks-en /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/hesperian_health /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/iicba /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/infonet /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/kaos-en /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/math_expression /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/medline_plus /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/medline_plus /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/musictheory /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/olpc /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/powertyping /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/practical_action /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/scratch /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/understanding_algebra /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/wikipedia_for_schools /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/rpi_guide /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/windows_apps /var/www/modules
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/asst_medical /var/www/modules
-rm -r /home/pi/rachelpiOS/www
+#!/bin/bash
+
+MODULES=( ebooks-en hesperian_health iicba infonet kaos-en math_expression \
+	  medline_plus musictheory olpc powertyping practical_action scratch \
+	  understanding_algebra wikipedia_for_schools rpi_guide windows_apps asst_medical )
+
+
+DEST_PATH=/var/www/modules
+RSYNC_SOURCE=rsync://dev.worldpossible.org/rachelmods/
+
+CMDNAME=$0
+
+if [ `id -u` != "0" ]; then
+    echo "$CMDNAME: This script must be run as root"
+    exit
+fi
+
+for module in ${MODULES[@]}; do
+    echo
+    echo "Syncing $module ..."
+    rsync -avz "${RSYNC_SOURCE}${module}" "$DEST_PATH"
+    if [ $? -ne 0 ]; then
+	sleep 1
+	echo "Copying of $module failed or was aborted.  Exiting."
+	exit
+    fi
+done
